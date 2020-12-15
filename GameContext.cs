510a1130ef93367f2045fa809c7759f176fa9a0b
jsakamoto <junichi.jp@gmail.com>
2020-12-15 19:36:05 +0900

    Define a rectangle of the snow man

--- GameContext.cs
+++ GameContext.cs
@@ -8,6 +8,8 @@ public class GameContext
 
     public Rectangle SnowFlakeRect = new(x: 0, y: 0, width: 48, height: 48);
 
+    public Rectangle SnowManRect = new(x: 0, y: 0, width: 64, height: 64);
+
     public Timer GameLoopTimer = new(interval: 30);
 
     private Random Random = new();
@@ -15,6 +17,8 @@ public class GameContext
     public GameContext()
     {
         this.ResetSnowFlakePos();
+        this.ResetSnowManPos();
+
         this.GameLoopTimer.Elapsed += GameLoopTimer_Elapsed;
         this.GameLoopTimer.Start();
     }
@@ -29,5 +33,11 @@ public class GameContext
     {
         this.SnowFlakeRect.Y = -this.SnowFlakeRect.Height;
         this.SnowFlakeRect.X = Random.Next(minValue: 0, maxValue: this.GameAreaSize.Width - this.SnowFlakeRect.Width);
+    }
+
+    private void ResetSnowManPos()
+    {
+        this.SnowManRect.X = (this.GameAreaSize.Width - this.SnowManRect.Width) / 2;
+        this.SnowManRect.Y = this.GameAreaSize.Height - this.SnowManRect.Height;
     }
 }
