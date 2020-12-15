14a1226a62959ae9fcb5dee8dcb9bf1114e44495
jsakamoto <junichi.jp@gmail.com>
2020-12-15 19:29:46 +0900

    Make snow flake to be falling down

--- GameContext.cs
+++ GameContext.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Drawing;
+using System.Timers;
 
 public class GameContext
 {
@@ -6,7 +8,22 @@ public class GameContext
 
     public Rectangle SnowFlakeRect = new(x: 0, y: 0, width: 48, height: 48);
 
+    public Timer GameLoopTimer = new(interval: 30);
+
     public GameContext()
     {
+        this.GameLoopTimer.Elapsed += GameLoopTimer_Elapsed;
+        this.GameLoopTimer.Start();
+    }
+
+    private void GameLoopTimer_Elapsed(object sender, EventArgs e)
+    {
+        this.SnowFlakeRect.Y += 10;
+        if (this.SnowFlakeRect.Y > this.GameAreaSize.Height) ResetSnowFlakePos();
+    }
+
+    private void ResetSnowFlakePos()
+    {
+        this.SnowFlakeRect.Y = -this.SnowFlakeRect.Height;
     }
 }
