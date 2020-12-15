d4d46e195edcc48489d5e2b515622e53768d2f41
jsakamoto <junichi.jp@gmail.com>
2020-12-15 19:50:50 +0900

    Add "Blazor HotKeys" package reference

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 namespace SnowCatch
 {
@@ -18,6 +19,7 @@ namespace SnowCatch
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddHotKeys();
 
             await builder.Build().RunAsync();
         }
