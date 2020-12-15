4fd6c1bfb2deadcc6df4ffcf227d30a6a558612d
jsakamoto <junichi.jp@gmail.com>
2020-12-15 18:25:29 +0900

    Define a rectangle of the snow flake

--- GameContext.cs
+++ GameContext.cs
@@ -4,6 +4,8 @@ public class GameContext
 {
     public Size GameAreaSize = new(width: 320, height: 480);
 
+    public Rectangle SnowFlakeRect = new(x: 0, y: 0, width: 48, height: 48);
+
     public GameContext()
     {
     }
