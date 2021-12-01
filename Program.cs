b037eadd22681adf2f9b77ea41ab1762ce7f098a
jsakamoto <junichi.jp@gmail.com>
2021-12-01 22:17:10 +0900

    Register the "GamepadList" service to the DI container

--- Program.cs
+++ Program.cs
@@ -7,5 +7,6 @@ builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddHotKeys();
+builder.Services.AddGamepadList();
 
 await builder.Build().RunAsync();
