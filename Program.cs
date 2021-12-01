f3f9c4437156c7c00cb58f167552151fe57cf252
jsakamoto <junichi.jp@gmail.com>
2021-12-01 21:58:29 +0900

    Change the code style of Program.cs to top level statement style.

--- Program.cs
+++ Program.cs
@@ -1,19 +1,11 @@
 ﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SnowCatch;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 
-namespace SnowCatch
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddHotKeys();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHotKeys();
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
