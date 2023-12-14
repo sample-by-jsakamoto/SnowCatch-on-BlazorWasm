e7e2b074049fc6ab26133fd06bc7b02dee3c7a68
jsakamoto <junichi.jp@gmail.com>
2023-12-14 20:38:50 +0900

    Update to .NET 8

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddHotKeys();
+builder.Services.AddHotKeys2();
 builder.Services.AddGamepadList();
 
 await builder.Build().RunAsync();
